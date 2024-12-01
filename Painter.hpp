2ef2a2e264f64d89ef8c58308f0086e51e7fb039
ArtiomCaracuianu <89927245+ArtiomCaracuianu@users.noreply.github.com>
2024-12-01 22:00:49 +0200

    Delete Painter.hpp

--- Painter.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "abstract_painter.hpp"
-
-class Painter : public AbstractPainter {
-public:
-    void DrawImage(const Point& topLeft, const Point& bottomRight, char** image) override; 
-    void WriteText(const Point& position, const char* text) override;                     
-};
