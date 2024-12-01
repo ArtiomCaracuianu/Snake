ce477fb94cf2289ecdf300f30cbed2b467eeaf28
ArtiomCaracuianu <89927245+ArtiomCaracuianu@users.noreply.github.com>
2024-12-01 22:05:07 +0200

    Update Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -1,15 +1,25 @@
-#pragma once
+#include "point.hpp"
 
-  struct Point {
-      int x;
-      int y;
+Point::Point(int _x, int _y) : x(_x), y(_y) {}
 
-    Point() : x(0), y(0) {}  
-    Point(int xPos, int yPos) : x(xPos), y(yPos) {}  
-  
-    Point operator=(const Point& other);  
-    bool operator==(const Point& other) const;  
+Point Point::operator=(const Point& other) {
+    x = other.x;
+    y = other.y;
+    return *this;
+}
 
-    friend std::istream& operator>>(std::istream& in, Point& point);  
-    friend std::ostream& operator<<(std::ostream& out, const Point& point);  
-};
+bool Point::operator==(const Point& other) const {
+    return x == other.x && y == other.y;
+}
+
+std::istream& operator>>(std::istream& in, Point& point) {
+    int x, y;
+    in >> x >> y;
+    point = Point(x, y);
+    return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const Point& point) {
+    out << point.x << " " << point.y;
+    return out;
+}
